2bdf9d0bad92b9bb49f206bd33c6824a8dc2b015
SinghVanshraj <vanshrajsingh28jul2004@gmail.com>
2026-03-19 20:53:30 +0530

    ninth commit

--- lib/features/location/viewmodels/location_viewmodel.dart
+++ lib/features/location/viewmodels/location_viewmodel.dart
@@ -1,1 +1,17 @@
-class LocationViewmodel {}
+import 'package:flutter/material.dart';
+import 'package:teacher_tracker/features/location/models/location_model.dart';
+
+class LocationViewmodel extends ChangeNotifier {
+  LocationModel? _locationModel;
+  bool _tracking = false;
+  bool get tracking => _tracking;
+  String? _error;
+  String? get error => _error;
+
+  Future<void> getCurrentLocation() {
+
+  }
+  Future<void> checkPermission() {
+    
+  }
+}
