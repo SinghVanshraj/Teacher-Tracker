9b66c74677907b0bb85db6c2af7d32c56b61832f
SinghVanshraj <vanshrajsingh28jul2004@gmail.com>
2026-04-01 23:55:13 +0530

    eleventh commit

--- lib/features/livelocation/live_location_view_model.dart
+++ lib/features/livelocation/live_location_view_model.dart
@@ -1,1 +1,34 @@
-class LiveLocationViewModel {}
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:teacher_tracker/core/services/web_socket_service.dart';
+
+class LiveLocationViewModel extends ChangeNotifier {
+  final WebSocketService _service = WebSocketService();
+
+  bool get _isConnected => _service.isConnected;
+
+  void connect(String url) {
+    _service.connect(url);
+    notifyListeners();
+  }
+
+  void sendLocation({
+    required String uid,
+    required double lat,
+    required double long,
+  }) {
+    final data = jsonEncode({"uid": uid, "lat": lat, "long": long});
+
+    return _service.send(data);
+  }
+
+  Stream get stream => _service.stream;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _service.disconnect();
+    super.dispose();
+  }
+}
